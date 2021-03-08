910065b4ac19f857dd5d2d5c691f661f33056665
pooya <info@centurytrail.com>
2021-03-08 01:41:32 -0800

    Initial Commit

---
+++ SwiftUIIrisColorChange/SwiftUIIrisColorChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIrisColorChangeApp.swift
+//  SwiftUIIrisColorChange
+//
+//  Created by Pooya on 2021-03-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIrisColorChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
